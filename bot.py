c03d2fc3efcf5efc819261cc18532f59ac174430
Shiekh Salman <165562509+ItSheikhSalman@users.noreply.github.com>
2026-06-20 19:52:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = "8720594618:AAG8qBE_4WqfZy4GHbwSrdn8254UKQiUzjw"
 
 valid_tokens = {
     "abc123": {"user": "salman", "verified": False}
